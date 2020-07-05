bf4b9a049f659d1483d296ef0cb50db729e67b8f
Fadlun Anaturdasa <f.anaturdasa@gmail.com>
2020-07-05 13:48:42 +0700

    connections

--- controllers/v2/api/OrderController.js
+++ controllers/v2/api/OrderController.js
@@ -61,7 +61,7 @@ router.get('/', authMiddleware, (req, res, next) => {
   // });
 
   var MongoClient = require('mongodb').MongoClient;
-  var url = 'mongodb://127.0.0.1/oshop';
+  var url = 'mongodb+srv://qaiserdev:m30ngm30ngAA@cluster0-51geo.mongodb.net/db_marketplace?retryWrites=true&w=majority';
 
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
